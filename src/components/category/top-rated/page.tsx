dd1464205ee2732c0d12d928af4855720b60e970
JinChaeKim <rlawlsco88@gmail.com>
2025-03-21 17:47:39 +0900

    feat:#4 디렉토리 설정

---
+++ src/components/category/top-rated/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const topRatedPage = () => {
+  return <div>topRatedPage</div>;
+};
+
+export default topRatedPage;
---
+++ src/components/category/upcoming/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const upcomingPage = () => {
+  return <div>upcomingPage</div>;
+};
+
+export default upcomingPage;
