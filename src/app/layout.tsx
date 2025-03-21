b50c1b8724f4c515055886bfbb3ba1d990fd404a
smileeeeely <minjoole.ivy@gmail.com>
2025-03-21 20:45:01 +0900

    chore:#5 레이아웃 기본 설정 및 이미지 포맷 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,1 +1,13 @@
+import './globals.css';
 
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='ko'>
+      <body className={`antialiased`}>{children}</body>
+    </html>
+  );
+}
