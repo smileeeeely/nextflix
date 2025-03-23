6cb26677de1ca8fb0a50916f055c779427c9b9eb
JinChaeKim <rlawlsco88@gmail.com>
2025-03-23 23:18:12 +0900

    refactor:#4 타입 수정 및 서버 액션 타입 적용

--- src/types/category/movie.ts
+++ src/types/category/movie.ts
@@ -18,4 +18,6 @@ export interface Movie {
 export interface ApiResponse {
   page: number;
   results: Movie[];
+  total_pages: number;
+  total_results: number;
 }
--- src/utils/tmdb/serverApi.ts
+++ src/utils/tmdb/serverApi.ts
@@ -1,4 +1,5 @@
 'use server';
+import { ApiResponse } from '@/types/category/movie';
 import { TMDB_NOW_PLAYING_API, TMDB_POPULAR_API, TMDB_TOP_RATED_API, TMDB_UPCOMING_API } from './tmdbURL';
 
 const options = {
@@ -10,27 +11,27 @@ const options = {
 };
 
 // NowPlaying
-export const getNowPlaying = async () => {
+export const getNowPlaying = async (): Promise<ApiResponse> => {
   const res = await fetch(TMDB_NOW_PLAYING_API, options);
   const data = await res.json();
   return data;
 };
 
 // Popular
-export const getPopular = async () => {
+export const getPopular = async (): Promise<ApiResponse> => {
   const res = await fetch(TMDB_POPULAR_API, options);
   const data = await res.json();
   return data;
 };
 
 // TopRated
-export const getTopRated = async () => {
+export const getTopRated = async (): Promise<ApiResponse> => {
   const res = await fetch(TMDB_TOP_RATED_API, options);
   const data = await res.json();
   return data;
 };
 
-export const getUpcoming = async () => {
+export const getUpcoming = async (): Promise<ApiResponse> => {
   const res = await fetch(TMDB_UPCOMING_API, options);
   const data = await res.json();
   return data;
