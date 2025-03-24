977eefdb5f9c560554c57120b8ab80e9e3a943f0
Young <dndud5182@gmail.com>
2025-03-24 16:12:21 +0900

    feat:#13 Intro 페이지에서 Header와 Footer 렌더링 조건 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,25 @@
+'use client'; // 클라이언트 컴포넌트로 변경
+
+import { usePathname } from 'next/navigation';
 import './globals.css';
+import Header from '@/components/commons/Header';
+import Footer from '@/components/commons/Footer';
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathName = usePathname(); // 현재 경로 가져오기
+  const isIntroPage = pathName === '/'; // Intro 페이지 여부
+
   return (
     <html lang='ko'>
-      <body className={`antialiased`}>{children}</body>
+      <body className={`antialiased`}>
+        {!isIntroPage && <Header />}
+        <main>{children}</main>
+        {!isIntroPage && <Footer />}
+      </body>
     </html>
   );
 }
