ec9d0401c7c7b8c8fb49510f024594cb5f76ce18
choichangyeon <changyon99@naver.com>
2025-03-24 10:12:49 +0900

    feat:#1 제작사 UI

--- src/components/detail/Info.tsx
+++ src/components/detail/Info.tsx
@@ -1,4 +1,7 @@
+import { IMG_PATH } from '@/app/detail/[id]/page';
 import { Movie } from '@/types/DetailMovie';
+import { formatOverview } from '@/utils/formatFunction';
+import Image from 'next/image';
 
 interface Props {
   movie: Movie;
@@ -6,7 +9,7 @@ interface Props {
 
 const Info = ({ movie }: Props) => {
   return (
-    <main className='flex flex-col'>
+    <main className='ml-[20px] flex flex-col'>
       <section className='flex w-full flex-row items-end bg-green-300'>
         <h1 className='mr-[20px] text-[30px]'>{movie.title}</h1>
 
@@ -16,8 +19,22 @@ const Info = ({ movie }: Props) => {
         })}
       </section>
       <section>
-        <p>{movie.overview}</p>
+        <h4 className='whitespace-pre-line'>{formatOverview(movie.overview)}</h4>
         {movie.homepage && <a href={movie.homepage}>{movie.title} 보러가기</a>}
+      </section>
+      {/* 제작 부분 */}
+      <section>
+        <h1 className='text-[30px]'>제작사</h1>
+        <div className='flex flex-row items-center gap-[30px]'>
+          {movie.production_companies.map((company) => {
+            // TODO: 추후 genre component 적용
+            return (
+              <div key={company.id}>
+                <Image src={IMG_PATH + company.logo_path} width={200} height={100} alt={company.name} />
+              </div>
+            );
+          })}
+        </div>
       </section>
     </main>
   );
