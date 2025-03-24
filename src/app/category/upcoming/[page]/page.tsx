a47c563e950c0dfaaa7470bed77babbb451104fb
JinChaeKim <rlawlsco88@gmail.com>
2025-03-24 09:20:56 +0900

    move:#4 동적 라우팅을 위한 폴더 이동

---
+++ src/app/category/upcoming/[page]/page.tsx
@@ -0,0 +1,23 @@
+import PageNationBtn from '@/components/pageNationBtn';
+import { Movie } from '@/types/category/movie';
+import { getUpcoming } from '@/utils/tmdb/serverApi';
+import React from 'react';
+
+// 서버 컴포넌트에서 쿼리 파라미터 가져오기
+const nowPlayingPage = async ({ params }: { params: { page?: string } }) => {
+  const page = parseInt(params.page || '1'); // 숫자로 변환
+  const data = await getUpcoming(page);
+  return (
+    <>
+      <div>
+        <h1>페이지 {page}</h1>
+        {data.results.map((movie: Movie) => {
+          return <div key={movie.id}>{movie.title}</div>;
+        })}
+      </div>
+      <PageNationBtn page={page} totalPages={data.total_pages} basePath={'category/upcoming'} />
+    </>
+  );
+};
+
+export default nowPlayingPage;
