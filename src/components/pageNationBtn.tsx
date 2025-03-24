f50b74f5f68d3376b674bfa4999b1f661c152601
JinChaeKim <rlawlsco88@gmail.com>
2025-03-24 09:25:31 +0900

    feat:#4 페이지네이션 버튼 구현

--- src/components/pageNationBtn.tsx
+++ src/components/pageNationBtn.tsx
@@ -1,11 +1,28 @@
 'use client';
 
-const PageNationBtn = () => {
+import { useRouter } from 'next/navigation';
+interface PageNationProps {
+  page: number;
+  totalPages: number;
+  basePath: string;
+}
+
+const PageNationBtn = ({ page, totalPages, basePath }: PageNationProps) => {
+  const router = useRouter();
+
+  const goToPage = (newPage: number) => {
+    if (newPage < 1 || newPage > totalPages) return;
+
+    router.push(`/${basePath}/${newPage}`); // 히스토리 스택(방문 기록)에 추가
+  };
+
   return (
     <div className='flex gap-8'>
-      <button>이전</button>
-      <p>현재페이지</p>
-      <button>다음</button>
+      {page > 1 && <button onClick={() => goToPage(page - 1)}>이전</button>}
+      <p>
+        페이지 {page} / {totalPages}
+      </p>
+      {page < totalPages && <button onClick={() => goToPage(page + 1)}>다음</button>}
     </div>
   );
 };
