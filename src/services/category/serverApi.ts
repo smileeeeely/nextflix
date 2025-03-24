1ebf7bdc7c20d43fe69a3b16fe8f285f32f95700
JinChaeKim <rlawlsco88@gmail.com>
2025-03-24 22:15:21 +0900

    refactor:#16 캐시 페이지 상수화

--- src/services/category/serverApi.ts
+++ src/services/category/serverApi.ts
@@ -3,6 +3,7 @@ import { ApiResponse } from '@/types/category/movie';
 import { TMDB_BASE_URL } from '@/constants/tmdbBaseUrl';
 
 export const ONE_DAY_SECONDS = 60 * 60 * 24;
+const CACHE_PAGE = 3;
 
 const options = {
   method: 'GET',
@@ -17,8 +18,8 @@ export const getNowPlaying = async (page: number = 1): Promise<ApiResponse> => {
   const TMDB_NOW_PLAYING_API = `${TMDB_BASE_URL}/now_playing?language=ko&page=${page}&region=KR`;
   const res = await fetch(TMDB_NOW_PLAYING_API, {
     ...options,
-    cache: page <= 3 ? 'force-cache' : 'no-store', // 1~3: ISR / 4~: SSR
-    next: page <= 3 ? { revalidate: ONE_DAY_SECONDS } : undefined, // 1~3: 하루
+    cache: page <= CACHE_PAGE ? 'force-cache' : 'no-store', // 1~3: ISR / 4~: SSR
+    next: page <= CACHE_PAGE ? { revalidate: ONE_DAY_SECONDS } : undefined, // 1~3: 하루
   });
   const data = await res.json();
   return data;
@@ -29,8 +30,8 @@ export const getPopular = async (page: number = 1): Promise<ApiResponse> => {
   const TMDB_POPULAR_API = `${TMDB_BASE_URL}/popular?language=ko&page=${page}&region=KR`;
   const res = await fetch(TMDB_POPULAR_API, {
     ...options,
-    cache: page <= 3 ? 'force-cache' : 'no-store',
-    next: page <= 3 ? { revalidate: ONE_DAY_SECONDS } : undefined,
+    cache: page <= CACHE_PAGE ? 'force-cache' : 'no-store',
+    next: page <= CACHE_PAGE ? { revalidate: ONE_DAY_SECONDS } : undefined,
   });
   const data = await res.json();
   return data;
@@ -41,8 +42,8 @@ export const getTopRated = async (page: number = 1): Promise<ApiResponse> => {
   const TMDB_TOP_RATED_API = `${TMDB_BASE_URL}/top_rated?language=ko&page=${page}&region=KR`;
   const res = await fetch(TMDB_TOP_RATED_API, {
     ...options,
-    cache: page <= 3 ? 'force-cache' : 'no-store',
-    next: page <= 3 ? { revalidate: ONE_DAY_SECONDS } : undefined,
+    cache: page <= CACHE_PAGE ? 'force-cache' : 'no-store',
+    next: page <= CACHE_PAGE ? { revalidate: ONE_DAY_SECONDS } : undefined,
   });
   const data = await res.json();
   return data;
@@ -52,8 +53,8 @@ export const getUpcoming = async (page: number = 1): Promise<ApiResponse> => {
   const TMDB_UPCOMING_API = `${TMDB_BASE_URL}/upcoming?language=ko&page=${page}&region=KR`;
   const res = await fetch(TMDB_UPCOMING_API, {
     ...options,
-    cache: page <= 3 ? 'force-cache' : 'no-store',
-    next: page <= 3 ? { revalidate: ONE_DAY_SECONDS } : undefined,
+    cache: page <= CACHE_PAGE ? 'force-cache' : 'no-store',
+    next: page <= CACHE_PAGE ? { revalidate: ONE_DAY_SECONDS } : undefined,
   });
   const data = await res.json();
   return data;
