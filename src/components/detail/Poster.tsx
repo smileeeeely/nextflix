5dc37cc6ba4c8f79993c969967d0298a8eeac733
choichangyeon <changyon99@naver.com>
2025-03-24 16:21:25 +0900

    feat:#1 Poster Shadow 설정

--- src/components/detail/Poster.tsx
+++ src/components/detail/Poster.tsx
@@ -7,8 +7,8 @@ interface Props {
 
 const Poster = ({ src, alt }: Props) => {
   return (
-    <section className='flex items-center justify-center'>
-      <Image src={src} width={300} height={400} alt={alt} />
+    <section className='mb-[30px] mt-[50px] flex items-center justify-center'>
+      <Image src={src} width={300} height={400} alt={alt} className='shadow-[0_30px_60px_-15px_rgba(0,0,0,0.3)]' />
     </section>
   );
 };
