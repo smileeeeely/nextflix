a763e5ada086332e2c3f77f354dc3e82fd3a696e
JinChaeKim <rlawlsco88@gmail.com>
2025-03-24 09:22:35 +0900

    refactor:#4 fetch 엔드포인트 상수화 -> 공통 부분 상수화 변경

--- src/utils/tmdb/tmdbURL.ts
+++ src/utils/tmdb/tmdbURL.ts
@@ -1,6 +1,1 @@
-// queryparams로 page값 받아오기
-
-export const TMDB_NOW_PLAYING_API = `https://api.themoviedb.org/3/movie/now_playing?language=ko&page=1&region=KR`;
-export const TMDB_POPULAR_API = 'https://api.themoviedb.org/3/movie/popular?language=ko&page=1&region=KR';
-export const TMDB_TOP_RATED_API = 'https://api.themoviedb.org/3/movie/top_rated?language=ko&page=1&region=KR';
-export const TMDB_UPCOMING_API = 'https://api.themoviedb.org/3/movie/upcoming?language=ko&page=1&region=KR';
+export const TMDB_BASE_URL = 'https://api.themoviedb.org/3/movie';
