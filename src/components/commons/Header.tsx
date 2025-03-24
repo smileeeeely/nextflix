73b245f76921a8e1062cde3a4001d8a2d42dd2b8
Young <dndud5182@gmail.com>
2025-03-24 17:33:27 +0900

    feat:[#13] Home 페이지로 이동하는 링크 추가

--- src/components/commons/Header.tsx
+++ src/components/commons/Header.tsx
@@ -1,11 +1,14 @@
+import Link from 'next/link';
 import MoviesSearch from './MoviesSearch';
 
 const Header = () => {
   return (
     <div className='flex gap-10 p-6 max-md:gap-6'>
-      <div className='flex items-center justify-center break-keep rounded-md border border-[#e6354f] px-8 py-4'>
-        로고
-      </div>
+      <Link href={'home'}>
+        <div className='flex items-center justify-center break-keep rounded-md border border-[#e6354f] px-8 py-4'>
+          로고
+        </div>
+      </Link>
       <MoviesSearch />
     </div>
   );
