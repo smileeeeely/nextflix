b98d1077ccf0467f7c9cfe0aca52859189763273
Young <dndud5182@gmail.com>
2025-03-24 20:29:06 +0900

    feat:[#13] Header 및 Footer를 별도 컴포넌트로 분리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,18 @@
-'use client'; // 클라이언트 컴포넌트로 변경
-
-import { usePathname } from 'next/navigation';
 import './globals.css';
-import Header from '@/components/commons/Header';
-import Footer from '@/components/commons/Footer';
+import HeaderLayout from './HeaderLayout';
+import FooterLayout from './FooterLayout';
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const pathName = usePathname(); // 현재 경로 가져오기
-  const isIntroPage = pathName === '/'; // Intro 페이지 여부
-
   return (
     <html lang='ko'>
       <body className={`antialiased`}>
-        {!isIntroPage && <Header />}
+        <HeaderLayout />
         <main>{children}</main>
-        {!isIntroPage && <Footer />}
+        <FooterLayout />
       </body>
     </html>
   );
