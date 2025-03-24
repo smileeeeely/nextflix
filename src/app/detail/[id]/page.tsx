97a986964c395c12c95a7dce212089f6909d7d12
choichangyeon <changyon99@naver.com>
2025-03-24 21:15:51 +0900

    feat:#10 Video fetch

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -3,7 +3,7 @@
 import Poster from '@/components/detail/Poster';
 import Info from '@/components/detail/Info';
 import { useEffect, useState } from 'react';
-import { getMovieDetails } from '@/services/serviceMovieDetails';
+import { getMovieDetails, getMovieVideo } from '@/services/serviceMovieDetails';
 import { Movie } from '@/types/DetailMovie';
 import { TMDB_IMG_URL } from '@/constants/tmdbConstants';
 
@@ -20,6 +20,7 @@ const DetailPage = ({ params }: Props) => {
   useEffect(() => {
     const dataFetch = async () => {
       const movie = await getMovieDetails(parseInt(params.id));
+      await getMovieVideo(parseInt(params.id));
       setSrc(`${TMDB_IMG_URL}/${movie.poster_path}`);
       setMovie(movie);
     };
--- src/services/serviceMovieDetails.ts
+++ src/services/serviceMovieDetails.ts
@@ -2,6 +2,7 @@
 
 import { TMDB_URL } from '@/constants/tmdbConstants';
 import { Movie } from '@/types/DetailMovie';
+import { MovieVideos, Video } from '@/types/Video';
 
 export const getMovieDetails = async (id: number): Promise<Movie> => {
   const res = await fetch(`${TMDB_URL}/${id}?language=ko`, {
@@ -14,4 +15,21 @@ export const getMovieDetails = async (id: number): Promise<Movie> => {
   const data: Movie = await res.json();
 
   return data;
+};
+
+export const getMovieVideo = async (id: number): Promise<Video | null> => {
+  const res = await fetch(`${TMDB_URL}/${id}/videos?language=ko`, {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
+    },
+  });
+  const data: MovieVideos = await res.json();
+  console.log(data);
+  if (data.results) {
+    return data.results[0];
+  }
+
+  return null;
 };
