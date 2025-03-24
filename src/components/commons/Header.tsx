386bda9a4a4a225af40210ff6913ff28025bc6e2
Young <dndud5182@gmail.com>
2025-03-24 17:25:05 +0900

    feat:[#13] Header 컴포넌트에서 IntroPage 렌더링

--- src/components/commons/Header.tsx
+++ src/components/commons/Header.tsx
@@ -1,7 +1,14 @@
-import IntroPage from '@/app/page';
+import MoviesSearch from './MoviesSearch';
 
 const Header = () => {
-  return <IntroPage />;
+  return (
+    <div className='flex gap-10 p-6 max-md:gap-6'>
+      <div className='flex items-center justify-center break-keep rounded-md border border-[#e6354f] px-8 py-4'>
+        로고
+      </div>
+      <MoviesSearch />
+    </div>
+  );
 };
 
 export default Header;
