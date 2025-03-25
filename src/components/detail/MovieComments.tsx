1ef4611677cbff1a1c44c3f2628ac48244c87937
choichangyeon <changyon99@naver.com>
2025-03-25 12:51:27 +0900

    feat:#11 댓글 컴포넌트 생성

---
+++ src/components/detail/MovieComments.tsx
@@ -0,0 +1,11 @@
+import { Comment } from '@/types/Comment';
+
+interface Props {
+  comments: Comment[];
+}
+
+const MovieComments = ({ comments }: Props) => {
+  return <section></section>;
+};
+
+export default MovieComments;
