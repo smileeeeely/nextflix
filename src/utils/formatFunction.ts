ee1beaa84c9eb3762fa6b81d6e854ec29c2bcf77
choichangyeon <changyon99@naver.com>
2025-03-25 14:54:01 +0900

    feat:#11 날짜 format 함수

--- src/utils/formatFunction.ts
+++ src/utils/formatFunction.ts
@@ -1,3 +1,17 @@
 export const formatOverview = (overview: string): string => {
   return overview.replace(/(?<!\.)\.(?!\.)/g, '.\n');
 };
+
+export const formatDate = (createAt: string): string => {
+  const date = new Date(createAt);
+
+  if (isNaN(date.getTime())) return '';
+
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const hour = date.getHours();
+  const min = date.getMinutes();
+
+  return `${year}년 ${month}월 ${day}일 ${hour}시 ${min}분`;
+};
