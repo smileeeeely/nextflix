6b65095d8a36ac558b7fb4d9cc5a01b266df8c18
choichangyeon <changyon99@naver.com>
2025-03-25 21:37:04 +0900

    check

--- src/components/commons/Header.tsx
+++ src/components/commons/Header.tsx
@@ -4,7 +4,7 @@ import MoviesSearch from './MoviesSearch';
 const Header = () => {
   return (
     <div className='mx-auto flex max-w-[1200px] gap-10 p-6 max-md:gap-6'>
-      <Link href={'home'}>
+      <Link href={'/category'}>
         <div className='flex items-center justify-center break-keep rounded-md border border-[#e6354f] px-8 py-4'>
           로고
         </div>
