3ecca20f97dcf1c4a6c3808eca8c368bb17e1708
choichangyeon <changyon99@naver.com>
2025-03-25 16:11:57 +0900

    feat:#11 댓글 UI 구성

--- src/components/detail/MovieComments.tsx
+++ src/components/detail/MovieComments.tsx
@@ -1,11 +1,41 @@
 import { Comment } from '@/types/Comment';
+import { formatDate } from '@/utils/formatFunction';
+import WrapperBox from '@/components/detail/WrapperBox';
+import { Button } from '@/components/ui/button';
 
 interface Props {
   comments: Comment[];
 }
 
 const MovieComments = ({ comments }: Props) => {
-  return <section></section>;
+  // 추후에 상태관리로 유저 로그인 정보 받아오기
+  const isLogin = true;
+
+  const mok_user = {
+    nickname: 'test1',
+    email: 'test1@test.com',
+    id: '6538aa12-c21b-416b-ac67-3c071829ecde',
+  };
+
+  return (
+    <section>
+      {comments.map((comment) => {
+        return (
+          <WrapperBox>
+            <p>{comment.content}</p>
+            <p className='flex justify-end'>
+              {comment.users.nickname} · {formatDate(comment.created_at)}
+            </p>
+            {isLogin && comment.user_id === mok_user.id ? (
+              <div className='mt-[10px] flex justify-end'>
+                <Button className='h-[25px] hover:bg-[#e6354f]'>삭제</Button>
+              </div>
+            ) : null}
+          </WrapperBox>
+        );
+      })}
+    </section>
+  );
 };
 
 export default MovieComments;
