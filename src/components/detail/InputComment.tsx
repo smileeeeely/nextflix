2567872dd6e632ea91c65d838bf031863c63f427
choichangyeon <changyon99@naver.com>
2025-03-25 17:14:29 +0900

    feat:#11 Input 컴포넌트 이벤트

--- src/components/detail/InputComment.tsx
+++ src/components/detail/InputComment.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
 import WrapperBox from '@/components/detail/WrapperBox';
 import { Textarea } from '@/components/ui/textarea';
 import { Button } from '@/components/ui/button';
+import { useState } from 'react';
 
 const InputComment = () => {
+  const [content, setContent] = useState<string>('');
+
+  const handleInsertComment = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // Insert 연결
+  };
+
   return (
     <WrapperBox>
-      <div className='flex flex-row items-end gap-[20px]'>
-        <Textarea></Textarea>
-        <Button className='min-h-[70px] w-[60px]'>입력</Button>
-      </div>
+      <form onSubmit={handleInsertComment} className='flex flex-row items-end gap-[20px]'>
+        <Textarea value={content} onChange={(e) => setContent(e.target.value)}></Textarea>
+        <Button type='submit' className='min-h-[70px] w-[60px]'>
+          입력
+        </Button>
+      </form>
     </WrapperBox>
   );
 };
