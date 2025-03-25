bf3605cee5efd6d395b42a1aa02317ed2fc5edb8
choichangyeon <changyon99@naver.com>
2025-03-25 14:56:08 +0900

    feat:#11 기능 주석 추가

--- src/utils/formatFunction.ts
+++ src/utils/formatFunction.ts
@@ -1,7 +1,9 @@
+// . 을 기준으로 줄넘김
 export const formatOverview = (overview: string): string => {
   return overview.replace(/(?<!\.)\.(?!\.)/g, '.\n');
 };
 
+// timestamp를 기준으로 날짜 포멧팅
 export const formatDate = (createAt: string): string => {
   const date = new Date(createAt);
 
