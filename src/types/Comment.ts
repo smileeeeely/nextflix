e82c12aaddc386f9c77d6184373273a8cc617e16
choichangyeon <changyon99@naver.com>
2025-03-25 12:50:01 +0900

    feat:#11 댓글 타입 지정

---
+++ src/types/Comment.ts
@@ -0,0 +1,12 @@
+export interface Nickname {
+  nickname: string;
+}
+
+export interface Comment {
+  id: string;
+  created_at: string;
+  user_id: string;
+  content: string;
+  movie_id: number;
+  users: Nickname;
+}
