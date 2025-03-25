2021aadec562340bd5251cc15e63943b29696c2c
JinChaeKim <rlawlsco88@gmail.com>
2025-03-25 22:56:37 +0900

    feat:#27 로그인 폼 구현

---
+++ src/components/commons/SignInForm.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import { setSupabaseSignIn } from '@/services/signIn';
+import { useRouter } from 'next/navigation';
+import { FieldValues, useForm } from 'react-hook-form';
+
+const SignInForm = () => {
+  const router = useRouter();
+  const { register, handleSubmit, formState } = useForm<FieldValues>({ mode: 'onBlur' });
+
+  const onSubmit = async (value: FieldValues): Promise<void> => {
+    try {
+      const { email, password } = value;
+      const data = await setSupabaseSignIn({ email, password });
+      console.log(data);
+      alert('로그인되었습니다.');
+      router.push('/home');
+    } catch (error) {
+      console.error('로그인 실패:', error);
+      alert('로그인에 실패하였습니다.');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className='mx-auto w-[350px] rounded-lg bg-gray-200 p-5'>
+      <div className='flex flex-col items-center'>
+        <label htmlFor='email' />
+        <input
+          className='m-3 w-[270px] rounded-md p-2'
+          type='email'
+          placeholder='email'
+          {...register('email', {
+            required: { value: true, message: '아이디를 입력하세요' },
+            pattern: {
+              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+              message: '이메일 형식으로 입력하세요.',
+            },
+          })}
+        />
+        {formState.errors.email && (
+          <span className='text-[14px] font-semibold'>{formState.errors.email.message as string}</span>
+        )}
+        <label htmlFor='password' />
+        <input
+          className='m-3 w-[270px] rounded-md p-2'
+          type='password'
+          placeholder='password'
+          {...register('password', {
+            required: { value: true, message: '비밀번호를 입력하세요' },
+            pattern: {
+              value: /^.{6,}$/,
+              message: '6자리 이상 입력하세요.',
+            },
+          })}
+        />
+        {formState.errors.password && (
+          <span className='text-[14px] font-semibold'>{formState.errors.password.message as string}</span>
+        )}
+        <button type='submit' className='my-5 h-[50px] w-[270px] items-center font-semibold text-white'>
+          로그인
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default SignInForm;
