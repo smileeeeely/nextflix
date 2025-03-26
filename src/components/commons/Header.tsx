8410910a6695afe0a959e5c018a7f949b0e8e41b
JinChaeKim <rlawlsco88@gmail.com>
2025-03-26 14:29:48 +0900

    refactor:#27 충돌 해결

--- src/components/commons/Header.tsx
+++ src/components/commons/Header.tsx
@@ -4,11 +4,7 @@ import MoviesSearch from './MoviesSearch';
 const Header = () => {
   return (
     <div className='mx-auto flex max-w-[1200px] gap-10 p-6 max-md:gap-6'>
-<<<<<<< HEAD
       <Link href={'/home'}>
-=======
-      <Link href={'/category'}>
->>>>>>> 689a58ef3cb61b1254cf40b37fbab767f1082f15
         <div className='flex items-center justify-center break-keep rounded-md border border-[#e6354f] px-8 py-4'>
           로고
         </div>
