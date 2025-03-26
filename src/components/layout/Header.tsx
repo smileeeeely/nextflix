2e5b30a4fe2da3f08723e58990665f535fd23fbe
lyra <mk.lyra.j@gmail.com>
2025-03-27 05:13:03 +0900

    refactor : #61 로고 이미지 크기 고정

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -13,11 +13,13 @@ const Header = () => {
       <nav className='mx-auto flex w-full max-w-[1200px] items-center justify-between gap-10 px-8'>
         {/* 로고, 카테고리 */}
         <section className='flex items-center gap-8 text-slate-50'>
-          {/* 로고 */}
+          {/* 로고 크기 고정 */}
           <h1>
-            <Link href={'/home'}>
-              <Image src='/images/logo.png' alt='NextFlix' width={50} height={35} className='object-cover' />
-            </Link>
+            <div className='relative h-[35px] w-[50px]'>
+              <Link href={'/home'}>
+                <Image src='/images/logo.png' alt='NextFlix' fill className='object-cover' />
+              </Link>
+            </div>
           </h1>
 
           {/* 카테고리 메뉴 컴포넌트 */}
