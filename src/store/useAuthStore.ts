8f5f81cfc4204391e1f16afedfc2a255be370db4
JinChaeKim <rlawlsco88@gmail.com>
2025-03-26 15:51:57 +0900

    feat:#36 store-토큰 유효 검증 함수 추가

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -1,4 +1,4 @@
-import { getUserByEmail, logOutSupabase } from '@/services/signIn';
+import { checkSession, getUserByEmail, logOutSupabase } from '@/services/signIn';
 import { create } from 'zustand';
 
 interface AuthState {
@@ -17,7 +17,7 @@ export const useAuthStore = create<AuthState>((set) => ({
   isSignedIn: false,
   user: null,
   // 로그인 함수
-  signIn: async (email) => {
+  signIn: async (email: string) => {
     const userData = await getUserByEmail(email); //supabase에서 userData 가져오기
     if (!userData) return;
     set({ isSignedIn: true, user: { email, nickname: userData.nickname } }); //로그인 한 유저 정보 담음
@@ -26,5 +26,15 @@ export const useAuthStore = create<AuthState>((set) => ({
   logout: async () => {
     await logOutSupabase();
     set({ isSignedIn: false, user: null });
+  },
+  // 토근 유효 검증 함수
+  checkAuth: async (email: string) => {
+    const userData = await checkSession();
+
+    if (userData) {
+      set({ isSignedIn: true, user: { email, nickname: userData.nickname } }); //유효할 때 유저 정보
+    } else {
+      set({ isSignedIn: false, user: null }); //유효하지 않을 때 로그아웃
+    }
   },
 }));
