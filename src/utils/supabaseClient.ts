226d948121b6032fb770404eb29cd0940571166b
JinChaeKim <rlawlsco88@gmail.com>
2025-03-27 00:55:14 +0900

    refactor:#47 next_public 적용

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,6 +1,7 @@
-import { createClient } from "@supabase/supabase-js";
+'use client';
+import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_PROJECT_URL = process.env.SUPABASE_URL!;
-const SUPABASE_ANON_KEY = process.env.SUPABASE_KEY!;
+const SUPABASE_PROJECT_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY!;
 
 export const supabase = createClient(SUPABASE_PROJECT_URL, SUPABASE_ANON_KEY);
