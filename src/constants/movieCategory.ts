2ea72306a89a2537bd23574551db3568b94213db
choichangyeon <changyon99@naver.com>
2025-03-26 20:03:11 +0900

    refactor:#39 BaseUrl 통합

--- src/constants/movieCategory.ts
+++ src/constants/movieCategory.ts
@@ -1,15 +1,13 @@
-export const BASE_URL = 'https://api.themoviedb.org/3/movie';
+import { TMDB_BASE_URL } from '@/constants/tmdbBaseUrl';
 
 export const CATEGORY_ENDPOINTS = {
-  now_playing: `${BASE_URL}/now_playing`,
-  popular: `${BASE_URL}/popular`,
-  top_rated: `${BASE_URL}/top_rated`,
-  upcoming: `${BASE_URL}/upcoming`,
+  now_playing: `${TMDB_BASE_URL}/now_playing`,
+  popular: `${TMDB_BASE_URL}/popular`,
+  top_rated: `${TMDB_BASE_URL}/top_rated`,
+  upcoming: `${TMDB_BASE_URL}/upcoming`,
 };
 
 export const API_LANGUAGE = 'ko-KR';
 export const API_PAGE = 1;
 export const CACHE_PAGE = 3;
 export const ONE_DAY_SECONDS = 60 * 60 * 24;
-
-export const IMG_BASE_URL = 'https://image.tmdb.org/';
