7a5c711988812b100259ea707fadf8ff5bc121be
choichangyeon <changyon99@naver.com>
2025-03-27 00:22:20 +0900

    feat:#52 북마크 alert 처리

--- src/components/detail/BookmarkBtn.tsx
+++ src/components/detail/BookmarkBtn.tsx
@@ -12,26 +12,28 @@ interface Props {
 
 const BookmarkBtn = ({ isBookmarked, onClick, movie_id }: Props) => {
   const { user } = useAuthStore();
+  const { ERROR, SUCCESS, INFO } = ALERT_TYPE;
 
   const handleIsBookmark = async () => {
     try {
       // 북마킹 되어있을 시
       if (isBookmarked) {
         await deleteMovieBookmark({ movie_id, user_id: user!.id });
+        openAlert({ type: INFO, title: '북마크', text: '북마크 삭제 완료!' });
       }
       // 북마킹이 안되어있을 시
       else {
         await insertMovieBookmark({ movie_id, user_id: user!.id });
+        openAlert({ type: SUCCESS, title: '북마크', text: '북마크 완료!' });
       }
       onClick();
     } catch (e: Error | any) {
-      const { ERROR } = ALERT_TYPE;
       openAlert({ type: ERROR, text: e.message });
     }
   };
 
   return (
-    <section onClick={handleIsBookmark} className='mr-[50px] flex flex-col items-end'>
+    <section onClick={handleIsBookmark} className='mb-[5px] mr-[50px] flex flex-col items-end'>
       <button className='h-[5 0px] w-[40px] text-black'>
         {isBookmarked ? (
           <Bookmark className='h-full w-full stroke-[#e6354f]' fill='#e6354f' />
