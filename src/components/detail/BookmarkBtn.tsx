7edd97374c189625f251e3aa7afed0f5ce93ea79
choichangyeon <changyon99@naver.com>
2025-03-26 10:40:19 +0900

    feat:#29 bookmark 버튼 UI

--- src/components/detail/BookmarkBtn.tsx
+++ src/components/detail/BookmarkBtn.tsx
@@ -1,5 +1,26 @@
+import { Bookmark } from 'lucide-react';
+import { useState } from 'react';
+
 const BookmarkBtn = () => {
-  return <div>BookmarkBtn</div>;
+  // bookmark 상태 넘겨 받아오기
+  const [isClicked, SetClicked] = useState(false);
+
+  const onClickedHandler = () => {
+    SetClicked((prev) => !prev);
+  };
+
+  return (
+    <section onClick={onClickedHandler} className='mr-[50px] flex flex-col items-end'>
+      <button className='h-[5 0px] w-[40px] text-black'>
+        {isClicked ? (
+          <Bookmark className='h-full w-full stroke-[#e6354f]' fill='#e6354f' />
+        ) : (
+          <Bookmark className='h-full w-full' />
+        )}
+      </button>
+      <p>북마크</p>
+    </section>
+  );
 };
 
 export default BookmarkBtn;
