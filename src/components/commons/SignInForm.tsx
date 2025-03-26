d81a4f1d43c9594c918cf3623e4b4ed8b0897ebe
JinChaeKim <rlawlsco88@gmail.com>
2025-03-27 05:02:56 +0900

    refactor:#47 로그인 전역 상태 관리

--- src/components/commons/SignInForm.tsx
+++ src/components/commons/SignInForm.tsx
@@ -15,6 +15,7 @@ const SignInForm = () => {
     try {
       const { email, password } = value;
       const data = await signInSupabase({ email, password }); //서버 액션 함수 호출
+
       if (data) {
         localStorage.setItem('auth_token', data.session.access_token); //로컬스토리지에 토큰 저장
         useAuthStore.getState().signIn(data.user.email as string); //로그인 상태 전역 업데이트
