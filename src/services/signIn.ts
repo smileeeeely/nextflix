a42bdbf06d2733b7d8db1c973486b099daab24cc
JinChaeKim <rlawlsco88@gmail.com>
2025-03-26 10:44:31 +0900

    move:#27 닉네임 가져오기 서버 액션으로 이동

--- src/services/signIn.ts
+++ src/services/signIn.ts
@@ -18,3 +18,14 @@ export const signInSupabase = async ({ email, password }: SignInProps) => {
   }
   return data;
 };
+
+// supabase에서 해당 email user의 nickname 가져오기
+export const getUserByEmail = async (email: string) => {
+  //single(): 하나의 행만 가져옴 (select만 할 시 배열)
+  const { data, error } = await supabase.from('users').select('nickname').eq('email', email).single();
+  if (error) {
+    console.error('유저 조회 실패', error.message);
+    return null;
+  }
+  return data;
+};
