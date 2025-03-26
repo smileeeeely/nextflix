3fdfeb43c0b9655130b32a3b0ac761cf5cd61acb
choichangyeon <changyon99@naver.com>
2025-03-27 02:53:32 +0900

    refactor:#52 react 수정

--- src/app/detail/[id]/error.tsx
+++ src/app/detail/[id]/error.tsx
@@ -1,6 +1,5 @@
 'use client';
 import { useRouter } from 'next/navigation';
-import React from 'react';
 
 const error = ({ error }: { error: Error & { digest?: string } }) => {
   const { push } = useRouter();
